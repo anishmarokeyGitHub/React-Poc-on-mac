9ee26582ea67e308c015f90e7478891cfbd9aad1
anishMarokey <anishmarokey@gmail.com>
2024-01-08 10:34:33 +0530

    dynamically added page

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,9 @@
 //PascalCase
 function Message() {
     //Jsx syntax :- Javascript XML
-    return <h1> Hello World</h1>;
+
+    const name = 'Anish'
+    return <h1> Hello {name} </h1>;
 }
 
 export default Message;
